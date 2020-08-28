e62a4daf3c53cbbbd7e69c03adb68a161a35b579
Tripti Saini <triptisaini123@gmail.com>
2020-08-28 11:29:43 +0530

    Updates

---
+++ precache-manifest.8136e6fbdba5ab08a6bcbf9dc07c351c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c1840f3db43e9bf5ba44f83c05fbe9",
+    "url": "/rizzle-clone/index.html"
+  },
+  {
+    "revision": "b145323fe4218c8457f9",
+    "url": "/rizzle-clone/static/css/main.62f006b6.chunk.css"
+  },
+  {
+    "revision": "1fcee51d032ceb78ee89",
+    "url": "/rizzle-clone/static/js/2.7ec36c18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rizzle-clone/static/js/2.7ec36c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b145323fe4218c8457f9",
+    "url": "/rizzle-clone/static/js/main.0b24d878.chunk.js"
+  },
+  {
+    "revision": "c833e9a4998f680a1a0c",
+    "url": "/rizzle-clone/static/js/runtime-main.1284d53e.js"
+  }
+]);
